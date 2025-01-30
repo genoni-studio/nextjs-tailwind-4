a50c15ff4a52280d11d801e235bb9702d60a17fd
Tom Genoni <tom@genoni.studio>
2025-01-30 09:14:52 -0800

    Update units

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className='flex flex-row'>
+    <div className='flex flex-col  sm:flex-row '>
       {/* This class will NOT work because the "spacing" theme has been disabled */}
-      <div className='mb-40'>
+      <div className='mb-sm pb-xxx-sm'>
         Photosynthesis is the ultimate source of energy and food for nearly all life on earth.
       </div>
       <div>
@@ -11,9 +11,7 @@ export default function Home() {
       <div>
         Photosynthesis is the ultimate source of energy and food for nearly all life on earth.
       </div>
-      <div>
-        Photosynthesis is the ultimate source of energy and food for nearly all life on earth.
-      </div>
+      <div>Photosynthesis is the ultimate source of energy and food for nearly spacing.</div>
     </div>
   );
 }
