d40e3344330c3187bf5d93addd703a54bb01d9a6
Tom Genoni <tom@genoni.studio>
2025-01-30 10:46:15 -0800

    Update deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         life on earth.
       </div>
       {/* This class will NOT work because the "spacing" theme has been disabled */}
-      <div className="pb-2">
+      <div className="pb-28">
         Photosynthesis is the ultimate source of energy and food for nearly all
         life on earth.
       </div>
